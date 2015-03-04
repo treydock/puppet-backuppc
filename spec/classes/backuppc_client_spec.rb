56e2838fe12b87ec8d678495dc84013132e2e900
Trey Dockendorf <treydock@gmail.com>
2015-03-04 14:27:53 -0600

    * Update rspec-puppet dependency to rspec-puppet 2.x * Replace rspec-system dependencies with beaker * Update unit tests

--- spec/classes/backuppc_client_spec.rb
+++ spec/classes/backuppc_client_spec.rb
@@ -4,18 +4,20 @@ describe 'backuppc::client', :type => :class do
 
   describe 'On an unknown operating system' do
     let(:facts) {{ :osfamily => 'Unknown' }}
-    it { should raise_error(Puppet::Error, /is not supported by this module/) }
+    it 'should raise an error' do
+      expect { should compile }.to raise_error(/is not supported by this module/)
+    end
   end
 
   context "On Ubuntu" do
     let(:facts) {{ :osfamily => 'Debian' }}
     let(:params) {{ :backuppc_hostname => 'backuppc.test.com' }}
-    it { should include_class("backuppc::params") }
+    it { should contain_class("backuppc::params") }
   end
 
   context "On RedHat" do
     let(:facts) {{ :osfamily => 'RedHat' }}
     let(:params) {{ :backuppc_hostname => 'backuppc.test.com' }}
-    it { should include_class("backuppc::params") }
+    it { should contain_class("backuppc::params") }
   end
 end
