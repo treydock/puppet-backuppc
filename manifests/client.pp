849cdf40a201d334af97a5294b48f8aeaae2523c
Trey Dockendorf <tdockendorf@osc.edu>
2022-08-16 10:02:04 -0400

    Add puppet-strings docs

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,197 +1,140 @@
-# == Class: backuppc::client
+# @summary Manage BackupPC client
 #
-# Configures a host for backup with the backuppc server.
-# Uses storedconfigs to provide the backuppc server with
-# required information.
-#
-# === Parameters
-#
-# For parameters that are not documented here see the server
-# manifest.
-#
-# [*ensure*]
-# Present or absent.
-#
-# [*system_account*]
-# Name of the user that will be created to allow backuppc
-# access to the system via ssh. This only applies to xfer
-# methods that require it. To override this set the system_account
-# to an empty string and configure access to the client yourself as
-# the default in the global config file (root) or change the
-# rsync_client_cmd or tar_client_cmd to suit your setup.
-#
-# [*system_home_directory*]
-# Absolute path to the home directory of the system account.
-#
-# [*system_additional_commands*]
-# Additional sudo commands to whitelist for the system_account. This
-# is useful if you need to execute any pre dump *scripts* on client before
-# backup. Please prefer system_additional_commands_noexec if you want
-# to whitelist a single command/binary since commands specified here
-# are going to be allowed without the NOEXEC options. See man sudoers
-# for details.
-#
-# [*system_additional_commands_noexec*]
-# Additional sudo commands to whitelist for the system_account. This
-# is useful if you need to execute any pre dump commands on client before
-# backup.
-#
-# [*manage_sudo*]
-# Boolean. Set to true to configure and install sudo and the
-# sudoers.d directory. Defaults to false and is only applied
-# if 1) xfer_method requires ssh access and 2) you're using
-# the system_account parameter.
-#
-# [*manage_rsync*]
-# Boolean. By default will install the rsync package. If you
-# manage this elsewhere set it to false. Defaults to true and
-# is only applied if 1) the xfer_method is rsync and 2) you're
-# using the system_account parameter.
-#
-# [*blackout_bad_ping_limit*]
-# To allow for periodic rebooting of a PC or other brief periods when a
-# PC is not on the network, a number of consecutive bad pings is allowed
-# before the good ping count is reset.
-#
-# [*ping_max_msec*]
-# Maximum latency between backuppc server and client to schedule
-# a backup. Default to 20ms.
-#
-# [*backups_disable*]
-# Disable all full and incremental backups. These settings are useful for a client that
-# is no longer being backed up (eg: a retired machine), but you wish to keep the last backups
-# available for browsing or restoring to other machines.
-#
-# [*xfer_method*]
-# What transport method to use to backup each host. Valid values are rsync,
-# rsyncd, tar and smb.
-#
-# [*xfer_loglevel*]
-# Level of verbosity in Xfer log files. 0 means be quiet, 1 will give will
-# give one line per file, 2 will also show skipped files on incrementals,
-# higher values give more output.
-#
-# [*smb_share_name*]
-# Name of the host share that is backed up when using SMB. This can be a string or an
-# array of strings if there are multiple shares per host.
-#
-# [*smb_share_username*]
-# Smbclient share user name. This is passed to smbclient's -U argument.
-#
-# [*smb_share_passwd*]
-# Smbclient share password. This is passed to smbclient via its PASSWD environment variable.
-#
-# [*smb_client_full_cmd*]
-# Command to run smbclient for a full dump.
-#
-# [*smb_client_incr_cmd*]
-# Command to run smbclient for an incremental dump.
-#
-# [*smb_client_restore_cmd*]
-# Command to run smbclient for a restore.
-#
-# [*tar_share_name*]
-# Which host directories to backup when using tar transport. This can be a string or an array
-# of strings if there are multiple directories to backup per host.
-#
-# [*tar_client_cmd*]
-# Command to run tar on the client. GNU tar is required. The default will run
-# the tar command as the user you specify in system_account.
-#
-# [*tar_full_args*]
-# Extra tar arguments for full backups.
-#
-# [*tar_incr_args*]
-# Extra tar arguments for incr backups.
-#
-# [*tar_client_restore_cmd*]
-# Full command to run tar for restore on the client. GNU tar is required.
-#
-# [*rsync_client_cmd*]
-# Full command to run rsync on the client machine. The default will run
-# the rsync command as the user you specify in system_account.
-#
-# [*rsync_client_restore_cmd*]
-# Full command to run rsync for restore on the client.
-#
-# [*rsync_share_name*]
-# Share name to backup. For $Conf{XferMethod} = "rsync" this should be a
-# file system path, eg '/' or '/home'.
-#
-# [*rsyncd_client_port*]
-# Rsync daemon port on host.
-#
-# [*rsyncd_user_name*]
-# Rsync daemon user name on host.
-#
-# [*rsyncd_passwd*]
-# Rsync daemon password on host.
-#
-# [*rsyncd_auth_required*]
-# Whether authentication is mandatory when connecting to the client's rsyncd. By default
-# this is on, ensuring that BackupPC will refuse to connect to an rsyncd on the client that
-# is not password protected.
-#
-# [*rsync_csum_cache_verify_prob*]
-# When rsync checksum caching is enabled (by adding the --checksum-seed=32761 option to
-# rsync_args), the cached checksums can be occasionally verified to make sure the file
-# contents matches the cached checksums.
-#
-# [*rsync_args*]
-# Arguments to rsync for backup.
-#
-# [*rsync_restore_args*]
-# Arguments to rsync for restore.
-#
-# [*backup_files_only*]
-# List of directories or files to backup. If this is defined, only these
-# directories or files will be backed up.
-#
-# [*backup_files_exclude*]
-# List of directories or files to exclude from the backup. For xfer_method smb,
-# only one of backup_files_exclude and backup_files_only can be specified per share.
-# If both are set for a particular share, then backup_files_only takes precedence and
-# backup_files_exclude is ignored.
-#
-# [*dump_pre_user_cmd*]
-# Optional command to run before a dump.
-#
-# [*dump_post_user_cmd*]
-# Optional command to run after a dump.
-#
-# [*dump_pre_share_cmd*]
-# Optional command to run before a dump of a share.
-#
-# [*dump_post_share_cmd*]
-# Optional command to run after a dump of a share.
-#
-# [*restore_pre_user_cmd*]
-# Optional command to run before a restore.
-#
-# [*restore_post_user_cmd*]
-# Optional command to run after a restore.
-#
-# [*user_cmd_check_status*]
-# Whether the exit status of each PreUserCmd and PostUserCmd is checked.
-#
-# [*hosts_file_dhcp*]
-# The way hosts are discovered has changed and now in most cases you should
-# use the default of 0 for the DHCP flag, even if the host has a dynamically
-# assigned IP address.
-#
-# [*hosts_file_most_users*]
-# Additional user names, separate by commas and with no white space, can be
-# specified. These users will also have full permission in the CGI interface
-# to stop/start/browse/restore backups for this host. These users will not be
-# sent email about this host.
-#
-# === Examples
-#
-#  See tests folder.
-#
-# === Authors
-#
-# Scott Barr <gsbarr@gmail.com>
+# @param backuppc_hostname
+#   The BackupPC hostname
+#   This is used to match which BackupPC server is authorized for backups
+# @param ensure
+#   Whether to add or remove BackupPC client resources
+# @param config_name
+#   Name of BackupPC client config
+# @param manage_system_account
+#   Manage the user account used to perform backups
+# @param system_account
+#   Account used to perform backups
+# @param system_account_home_directory
+#   The home directory of backup user
+# @param system_account_uid
+#   Backup user UID
+# @param system_account_gid
+#   Backup user and group GID
+# @param system_additional_commands
+#   Authorize commands for backup user via sudo
+# @param system_additional_commands_noexec
+#   Authorize commands for backup user via sudo with NOEXEC
+# @param manage_sudo
+#   Include sudo class
+# @param manage_rsync
+#   Manage rsync package
+# @param rsync_path
+#   rsync path
+# @param tar_path
+#   tar path
+# @param client_name_alias
+#   See config.pl ClientNameAlias
+# @param xfer_method
+#   See config.pl XferMethod
+# @param xfer_loglevel
+#   See config.pl XferLogLevel
+# @param backups_disable
+#   See config.pl BackupsDisable
+# @param full_period
+#   See config.pl FullPeriod
+# @param incr_period
+#   See config.pl IncrPeriod
+# @param full_keep_cnt
+#   See config.pl FullKeepCnt
+# @param full_keep_cnt_min
+#   See config.pl FullKeepCntMin
+# @param full_age_max
+#   See config.pl FullAgeMax
+# @param incr_keep_cnt
+#   See config.pl IncrKeepCnt
+# @param incr_keep_cnt_min
+#   See config.pl IncrKeepCntMin
+# @param incr_age_max
+#   See config.pl IncrAgeMax
+# @param blackout_bad_ping_limit
+#   See config.pl BlackoutBadPingLimit
+# @param ping_max_msec
+#   See config.pl PingMaxMsec
+# @param blackout_good_cnt
+#   See config.pl BlackoutGoodCnt
+# @param backup_files_only
+#   See config.pl BackupFilesOnly
+# @param backup_files_exclude
+#   See config.pl BackupFilesExclude
+# @param smb_share_name
+#   See config.pl SmbShareName
+# @param smb_share_username
+#   See config.pl SmbShareUserName
+# @param smb_share_passwd
+#   See config.pl SmbSharePasswd
+# @param smb_client_full_cmd
+#   See config.pl SmbClientFullCmd
+# @param smb_client_incr_cmd
+#   See config.pl SmbClientIncrCmd
+# @param smb_client_restore_cmd
+#   See config.pl SmbClientRestoreCmd
+# @param tar_share_name
+#   See config.pl TarShareName
+# @param tar_client_cmd
+#   See config.pl TarClientCmd
+# @param tar_full_args
+#   See config.pl TarFullArgs
+# @param tar_incr_args
+#   See config.pl TarIncrArgs
+# @param tar_client_restore_cmd
+#   See config.pl TarClientRestoreCmd
+# @param rsync_client_path
+#   See config.pl RsyncClientPath
+# @param rsync_ssh_args
+#   See config.pl RsyncSshArgs
+# @param rsync_args
+#   See config.pl RsyncArgs
+# @param rsync_args_extra
+#   See config.pl RsyncArgsExtra
+# @param rsync_restore_args
+#   See config.pl RsyncRestoreArgs
+# @param rsync_restore_args_extra
+#   See config.pl RsyncRestoreArgsExtra
+# @param rsync_share_name
+#   See config.pl RsyncShareName
+# @param rsyncd_client_port
+#   See config.pl RsyncdClientPort
+# @param rsyncd_user_name
+#   See config.pl RsyncdUserName
+# @param rsyncd_passwd
+#   See config.pl RsyncdPasswd
+# @param dump_pre_user_cmd
+#   See config.pl DumpPreUserCmd
+# @param dump_post_user_cmd
+#   See config.pl DumpPostUserCmd
+# @param dump_pre_share_cmd
+#   See config.pl DumpPreShareCmd
+# @param dump_post_share_cmd
+#   See config.pl DumpPostShareCmd
+# @param restore_pre_user_cmd
+#   See config.pl RestorePreUserCmd
+# @param restore_post_user_cmd
+#   See config.pl RestorePostUserCmd
+# @param user_cmd_check_status
+#   See config.pl UserCmdCheckStatus
+# @param email_notify_min_days
+#   See config.pl EMailNotifyMinDays
+# @param email_from_user_name
+#   See config.pl EMailFromUserName
+# @param email_admin_user_name
+#   See config.pl EMailAdminUserName
+# @param email_notify_old_backup_days
+#   See config.pl EMailNotifyOldBackupDays
+# @param hosts_file_dhcp
+#   Whether to use enable DHCP in hosts file
+# @param hosts_file_user
+#   Hosts file user
+# @param hosts_file_more_users
+#   Hosts file more users
+# @param export_sshkey
+#   Set whether to export the SSH key for the backup client to BackupPC server
 #
 class backuppc::client (
   Stdlib::Host $backuppc_hostname,
--- manifests/server.pp
+++ manifests/server.pp
@@ -1,175 +1,152 @@
-# == Class: backuppc::server
+# @summary Manage BackPC server
 #
-# This module manages backuppc
-#
-# === Parameters
-#
-# [*ensure*]
-# Present or absent
-#
-# [*service_enable*]
-# Boolean. Will enable service at boot
-# and ensure a running service.
-#
-# [*wakup_schedule*]
-# Times at which we wake up, check all the PCs,
-# and schedule necessary backups. Times are measured
-# in hours since midnight. Can be fractional if
-# necessary (eg: 4.25 means 4:15am).
-#
-# [*max_backups*]
-# Maximum number of simultaneous backups to run. If
-# there are no user backup requests then this is the
-# maximum number of simultaneous backups.
-#
-# [*max_user_backups*]
-# Additional number of simultaneous backups that users
-# can run. As many as $Conf{MaxBackups} + $Conf{MaxUserBackups}
-# requests can run at the same time.
-#
-# [*max_pending_cmds*]
-# Maximum number of pending link commands. New backups will only
-# be started if there are no more than $Conf{MaxPendingCmds} plus
-# $Conf{MaxBackups} number of pending link commands, plus running
-# jobs. This limit is to make sure BackupPC doesn't fall too far
-# behind in running BackupPC_link commands.
-#
-# [*max_backup_pc_nightly_jobs*]
-# How many BackupPC_nightly processes to run in parallel. Each night,
-# at the first wakeup listed in $Conf{WakeupSchedule}, BackupPC_nightly
-# is run. Its job is to remove unneeded files in the pool, ie: files that
-# only have one link. To avoid race conditions, BackupPC_nightly and BackupPC_link
-# cannot run at the same time. Starting in v3.0.0, BackupPC_nightly can run
-# concurrently with backups (BackupPC_dump).
-#
-# [*backup_pc_nightly_period*]
-# How many days (runs) it takes BackupPC_nightly to traverse the entire pool.
-# Normally this is 1, which means every night it runs, it does traverse the entire
-# pool removing unused pool files.
-#
-# [*max_old_log_files*]
-# Maximum number of log files we keep around in log directory. These files are aged
-# nightly. A setting of 14 means the log directory will contain about 2 weeks of old
-# log files, in particular at most the files LOG, LOG.0, LOG.1, ... LOG.13 (except today's
-# LOG, these files will have a .z extension if compression is on).
-#
-# [*df_max_usage_pct*]
-# Maximum threshold for disk utilization on the __TOPDIR__ filesystem. If the output
-# from $Conf{DfPath} reports a percentage larger than this number then no new regularly
-# scheduled backups will be run. However, user requested backups (which are usually
-# incremental and tend to be small) are still performed, independent of disk usage. Also,
-# currently running backups will not be terminated when the disk usage exceeds this number.
-#
-# [*trash_clean_sleep_sec*]
-# How long BackupPC_trashClean sleeps in seconds between each check of the trash directory.
-#
-# [*dhcp_address_ranges*]
-# List of DHCP address ranges we search looking for PCs to backup. This is an array of
-# hashes for each class C address range. This is only needed if hosts in the conf/hosts
-# file have the dhcp flag set.
-#
-# [*full_period*]
-# Minimum period in days between full backups. A full dump will only be done if at least
-# this much time has elapsed since the last full dump, and at least $Conf{IncrPeriod}
-# days has elapsed since the last successful dump.
-#
-# [*full_keep_cnt*]
-# Number of full backups to keep.
-#
-# [*full_age_max*]
-# Very old full backups are removed after $Conf{FullAgeMax} days. However, we keep
-# at least $Conf{FullKeepCntMin} full backups no matter how old they are.
-#
-# [*incr_period*]
-# Minimum period in days between incremental backups (a user requested incremental
-# backup will be done anytime on demand).
-#
-# [*incr_keep_cnt*]
-# Number of incremental backups to keep.
-#
-# [*incr_age_max*]
-# Very old incremental backups are removed after $Conf{IncrAgeMax} days. However,
-# we keep at least $Conf{IncrKeepCntMin} incremental backups no matter how old
-# they are.
-#
-# [*incr_levels*]
-# A full backup has level 0. A new incremental of level N will backup all files
-# that have changed since the most recent backup of a lower level.
-#
-# [*partial_age_max*]
-# A failed full backup is saved as a partial backup. The rsync XferMethod can
-# take advantage of the partial full when the next backup is run. This parameter
-# sets the age of the partial full in days: if the partial backup is older than
-# this number of days, then rsync will ignore (not use) the partial full when the
-# next backup is run. If you set this to a negative value then no partials will be
-# saved. If you set this to 0, partials will be saved, but will not be used by the
-# next backup.
-#
-# [*incr_fill*]
-# Boolean. Whether incremental backups are filled. "Filling" means that the most recent fulli
-# (or filled) dump is merged into the new incremental dump using hardlinks. This
-# makes an incremental dump look like a full dump.
-#
-# [*restore_info_keep_cnt*]
-# Number of restore logs to keep. BackupPC remembers information about each restore
-# request. This number per client will be kept around before the oldest ones are pruned.
-#
-# [*archive_info_keep_cnt*]
-# Number of archive logs to keep. BackupPC remembers information about each archive request.
-# This number per archive client will be kept around before the oldest ones are pruned.
-#
-# [*blackout_good_cnt*]
-# PCs that are always or often on the network can be backed up after hours, to reduce PC,
-# network and server load during working hours. For each PC a count of consecutive good
-# pings is maintained. Once a PC has at least $Conf{BlackoutGoodCnt} consecutive good pings
-# it is subject to "blackout" and not backed up during hours and days specified by $Conf{BlackoutPeriods}.
-#
-# [*blackout_zero_files_is_fatal*]
-# Boolean. A backup of a share that has zero files is considered fatal. This is used to catch miscellaneous Xfer
-# errors that result in no files being backed up. If you have shares that might be
-# empty (and therefore an empty backup is valid) you should set this to false.
-#
-# [*email_notify_min_days*]
-# Minimum period between consecutive emails to a single user. This tries to keep annoying email to users to
-# a reasonable level.
-#
-# [*email_from_user_name*]
-# Name to use as the "from" name for email.
-#
-# [*email_admin_user_name*]
-# Destination address to an administrative user who will receive a nightly email with warnings and errors.
-#
-# [*email_notify_old_backup_days*]
-# How old the most recent backup has to be before notifying user. When there have been no backups in this
-# number of days the user is sent an email.
-#
-# [*email_headers*]
-# Additional email headers.
-#
-# [*apache_configuration*]
-# Boolean. Whether to install the apache configuration file that creates an alias for the /backuppc url.
-# Disable this if you intend to install backuppc as a virtual host yourself.
-#
-# [*apache_allow_from*]
-# A space seperated list of hostnames, ip addresses and networks that are permitted to
-# access the backuppc interface.
-#
-# [*apache_require_ssl*]
-# This directive forbids access unless HTTP over SSL (i.e. HTTPS) is used. Relies on mod_ssl.
-#
-# [*backuppc_password*]
-# Password for the backuppc user used to access the web interface.
-#
-# [*topdir*]
-# Overwrite package default location for backuppc.
-#
-# === Examples
-#
-#  See tests folder.
-#
-# === Authors
-#
-# Scott Barr <gsbarr@gmail.com>
+# @param package_name
+#   Package name
+# @param manage_repo
+#   Manage repo
+#   Only applies to EL systems to manage EPEL
+# @param service_name
+#   Service name
+# @param config_dir
+#   Config directory
+# @param replace_config
+#   Set if replace BackupPC config.pl
+# @param manage_topdir
+#   Manage BackupPC topdir
+# @param topdir
+#   Path to topdir
+# @param install_dir
+#   BackupPC install directory
+# @param log_dir
+#   Log directory
+# @param run_dir
+#   Run dir
+# @param df_path
+#   Path to df
+# @param cat_path
+#   Path to cat
+# @param par_path
+#   Path to par2
+# @param gzip_path
+#   Path to gzip
+# @param bzip2_path
+#   Path to bzip2
+# @param rsync_path
+#   Path to rsync
+# @param tar_path
+#   Path to tar
+# @param rsync_bpc_path
+#   Path to rsync_bpc
+# @param ping_path
+#   Path to ping
+# @param ping6_path
+#   Path to ping6
+# @param cgi_dir
+#   CGI directory
+# @param cgi_image_dir
+#   CGI image directory
+# @param cgi_image_dir_url
+#   CGI image directory URL
+# @param apache_group
+#   Apache group
+# @param server_host
+#   config.pl ServerHost
+# @param wakeup_schedule
+#   config.pl WakeupSchedule
+# @param pool_v3_enabled
+#   config.pl PoolV3Enabled
+# @param max_backups
+#   config.pl MaxBackups
+# @param max_user_backups
+#   config.pl MaxUserBackups
+# @param max_pending_cmds
+#   config.pl MaxPendingCmds
+# @param max_backup_pc_nightly_jobs
+#   config.pl MaxBackupPCNightlyJobs
+# @param backup_pc_nightly_period
+#   config.pl BackupPCNightlyPeriod
+# @param pool_size_nightly_update_period
+#   config.pl PoolSizeNightlyUpdatePeriod
+# @param pool_nightly_digest_check_percent
+#   config.pl PoolNightlyDigestCheckPercent
+# @param ref_cnt_fsck
+#   config.pl RefCntFsck
+# @param max_old_log_files
+#   config.pl MaxOldLogFiles
+# @param df_max_usage_pct
+#   config.pl DfMaxUsagePct
+# @param df_max_inode_usage_pct
+#   config.pl DfMaxInodeUsagePct
+# @param dhcp_address_ranges
+#   config.pl DHCPAddressRanges
+# @param full_period
+#   config.pl FullPeriod
+# @param incr_period
+#   config.pl IncrPeriod
+# @param fill_cycle
+#   config.pl FillCycle
+# @param full_keep_cnt
+#   config.pl FullKeepCnt
+# @param full_age_max
+#   config.pl FullAgeMax
+# @param incr_keep_cnt
+#   config.pl IncrKeepCnt
+# @param incr_age_max
+#   config.pl IncrAgeMax
+# @param restore_info_keep_cnt
+#   config.pl RestoreInfoKeepCnt
+# @param archive_info_keep_cnt
+#   config.pl ArchiveInfoKeepCnt
+# @param blackout_good_cnt
+#   config.pl BlackoutGoodCnt
+# @param blackout_periods
+#   config.pl BlackoutPeriods
+# @param blackout_zero_files_is_fatal
+#   config.pl BackupZeroFilesIsFatal
+# @param rsync_cmd_default_user
+#   Default user for rsync backups
+# @param rsync_cmd_with_sudo
+#   Use sudo with rsync command
+# @param rsync_args
+#   config.pl RsyncArgs
+# @param rsync_args_extra
+#   config.pl RsyncArgsExtra
+# @param rsync_full_args_extra
+#   config.pl RsyncFullArgsExtra
+# @param rsync_incr_args_extra
+#   config.pl RsyncIncrArgsExtra
+# @param rsync_restore_args
+#   config.pl RsyncRestoreArgs
+# @param rsync_restore_args_extra
+#   config.pl RsyncRestoreArgsExtra
+# @param compress_level
+#   config.pl CompressLevel
+# @param email_notify_min_days
+#   config.pl EMailNotifyMinDays
+# @param email_from_user_name
+#   config.pl EMailFromUserName
+# @param email_admin_user_name
+#   config.pl EMailAdminUserName
+# @param email_user_dest_domain
+#   config.pl EMailUserDestDomain
+# @param email_notify_old_backup_days
+#   config.pl EMailNotifyOldBackupDays
+# @param email_headers
+#   config.pl EMailHeaders
+# @param cgi_admin_user_group
+#   config.pl CgiAdminUserGroup
+# @param cgi_admin_users
+#   config.pl CgiAdminUsers
+# @param cgi_url
+#   config.pl CgiURL
+# @param manage_user
+#   Manage BackupPC user and group
+# @param user_uid
+#   BackupPC UID
+# @param user_shell
+#   BackupPC user shell
+# @param user_gid
+#   BackupPC user and group GID
 #
 class backuppc::server (
   String $package_name = 'BackupPC',
